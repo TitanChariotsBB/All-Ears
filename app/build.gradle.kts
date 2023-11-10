b0968c0d12049f64c3395a73c4e951a32d6f95ce
TitanChariotsBB <cmagwoodva@gmail.com>
2023-11-10 09:44:38 -0500

    Fixed build settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.allears"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.allears"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
