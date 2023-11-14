abd876f3e5676419efc5465caaa402e8799ea82f
radunsb <radunsb@gmail.com>
2023-11-14 14:06:47 -0500

    Added sound and scoring for Note section

--- app/src/main/java/com/example/allears/ui/screens/NoteScreen.kt
+++ app/src/main/java/com/example/allears/ui/screens/NoteScreen.kt
@@ -1,7 +1,9 @@
 package com.example.allears.ui.screens
 
+import android.content.Context
 import android.graphics.Outline
 import android.graphics.drawable.shapes.Shape
+import android.media.MediaPlayer
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -16,20 +18,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.allears.R
 import com.example.allears.ui.models.NoteVM
+import com.example.allears.ui.theme.NoteConverter
+import kotlin.random.Random
 
 @Composable
 fun NoteScreen(VM : NoteVM, modifier: Modifier = Modifier) {
     val uiState by VM.noteUIState.collectAsState()
+    var currentNote by rememberSaveable{mutableStateOf('H')}
+    var hasNote by rememberSaveable{mutableStateOf(false)}
+    var score by rememberSaveable{mutableStateOf(0)}
+    val context = LocalContext.current
 
     /*
     * TODO:
@@ -53,8 +66,13 @@ fun NoteScreen(VM : NoteVM, modifier: Modifier = Modifier) {
             .padding(vertical = 64.dp, horizontal = 32.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = "Note: $currentNote Score: ${uiState.score}")
         OutlinedButton(
-            onClick = { /*TODO*/ },
+            onClick = {
+                val enabledNotes = ((uiState.noteMap.filter { it.value }).keys).toList()
+                currentNote = enabledNotes[Random.nextInt(0, enabledNotes.size)]
+                if(!hasNote) hasNote = true
+                playNotes(context, currentNote) },
             modifier = modifier
                 .padding(32.dp)
                 .size(172.dp),
@@ -67,31 +85,38 @@ fun NoteScreen(VM : NoteVM, modifier: Modifier = Modifier) {
             )
         }
         Column {
-            ShowAnswerButtons(VM)
+            uiState.notes.forEach{
+                if(uiState.noteMap.get(it) == true) {
+                    AnswerButton(text = it.toString(), onClick = {
+                        if(it == currentNote && hasNote) {
+                            VM.scoreChanged(uiState.score + 1)
+                            hasNote = false
+                        }
+                    })
+                }
+            }
         }
     }
 }
 
-@Composable
-fun ShowAnswerButtons(VM : NoteVM){
-    val uiState by VM.noteUIState.collectAsState()
-    uiState.notes.forEach{
-        if(uiState.noteMap.get(it) == true) {
-            AnswerButton(text = it.toString(), onClick = {/*TODO*/ })
-        }
-    }
-}
 
 @Composable
 fun AnswerButton(text: String, onClick: ()->Unit, modifier: Modifier = Modifier) {
     OutlinedButton(
-        onClick = { onClick },
+        onClick = { onClick() },
         modifier = modifier
             .padding(8.dp),
         shape = RoundedCornerShape(16.dp)
     ) {
         Text(text = text)
     }
+}
+
+fun playNotes(context : Context, currentNote : Char){
+    val converter : NoteConverter = NoteConverter()
+    val fileName = currentNote.toString().lowercase() + "5";
+    val mediaPlayer = MediaPlayer.create(context, converter.converterMap.get(fileName)!!)
+    mediaPlayer.start()
 }
 
 //@Preview
